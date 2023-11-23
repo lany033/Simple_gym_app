80b4ef18b6529ade9338c58de802015abc88840e
Melanie <mmantilla930@gmail.com>
2023-11-23 11:39:58 -0500

    add sheet in allequipment button

--- app/src/main/java/com/lifebetter/simplegymapp/ui/screens/exercises/ExercisesScreen.kt
+++ app/src/main/java/com/lifebetter/simplegymapp/ui/screens/exercises/ExercisesScreen.kt
@@ -14,23 +14,41 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.lifebetter.simplegymapp.model.ExercisesRepository
 import com.lifebetter.simplegymapp.ui.components.MyTopWithIconsBar
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ExercisesScreen() {
+
+    val sheetState = rememberModalBottomSheetState()
+    val scope = rememberCoroutineScope()
+    var showBottomSheet by rememberSaveable {
+        mutableStateOf(false)
+    }
 
     val viewModel: ExercisesViewModel = viewModel {
         ExercisesViewModel(ExercisesRepository())
@@ -63,7 +81,7 @@ fun ExercisesScreen() {
                     )
                 })
             Row() {
-                Button(onClick = { /*TODO*/ },shape = RoundedCornerShape(4.dp), modifier = Modifier
+                Button(onClick = { showBottomSheet = true }, shape = RoundedCornerShape(4.dp), modifier = Modifier
                     .weight(1f)
                     .size(40.dp)) {
                     Text(text = "All equipment", fontSize = 16.sp)
@@ -75,6 +93,30 @@ fun ExercisesScreen() {
                     Text(text = "All Muscles", fontSize = 16.sp)
                 }
             }
+
+            if (showBottomSheet){
+                ModalBottomSheet(onDismissRequest = { showBottomSheet = false }, sheetState = sheetState) {
+                    Column {
+                        Row(modifier = Modifier.fillMaxWidth(),horizontalArrangement = Arrangement.Center) {
+                            Text(text = "Equipment", fontSize = 16.sp, modifier = Modifier.padding(15.dp))
+                        }
+                        Divider(color = Color.LightGray, thickness = 0.5.dp)
+                        Spacer(modifier = Modifier.size(20.dp))
+                        Button(onClick = {
+                            scope.launch { sheetState.hide() }.invokeOnCompletion {
+                                if (!sheetState.isVisible) {
+                                    showBottomSheet = false
+                                }
+                            }
+                        }) {
+                            Text("Hide bottom sheet")
+                        }
+                        Spacer(modifier = Modifier.size(20.dp))
+                    }
+
+                }
+            }
+
             Text(text = "Results")
             LazyColumn {
                 items(state.exercise.size){i ->
